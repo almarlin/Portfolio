97efdb85e139aba654369f99285a31c701173119
Alvaro Martinez <alvaro.martinez.lineros@gmail.com>
2023-11-10 14:26:26 +0100

    Prueba fondos

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
     <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    
     <link rel="stylesheet" href="css/style.css">
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
@@ -47,15 +48,17 @@
 
     </header>
     <main>
-        <section class="container">
+
+        <section class="container-fluid">
+
             <div class="row justify-content-center align-items-start g-2">
                 <div class="col-6 col-md-5 col-xl-3 mb-2">
                     <img src="resources/angronWeb.png" class="img-fluid rounded-2" alt="Álvaro">
                 </div>
                 <div class="container col-12 col-md-12 col-xl-9">
                     <div class="row justify-content-center align-items-center gap-5">
-                        <div class="col-11 col-md-7 bg-greenPrimary rounded-5 p-3">
-                            <h1 class="display-6 text-light text-center">Álvaro Martínez Lineros</h1>
+                        <div class="col-11 col-md-7 bg-greenPrimary  rounded-5 p-3">
+                            <h1 class="display-6 text-light nombre text-center">Álvaro Martínez Lineros</h1>
                         </div>
                         <div class="col-1 col-md-1 col-xl-1 rounded-5 p-3 bg-greenPrimary">
                             <a href="">Github</a>
@@ -73,6 +76,7 @@
 
             </div>
         </section>
+
     </main>
     <footer>
         <!-- place footer here -->
