41fe48cc27938b54fe49e36f048cbe9ecd18db4b
Alvaro Martinez <alvaro.martinez.lineros@gmail.com>
2023-11-13 09:53:23 +0100

    Land & inicio circulos

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 
     <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    
+
     <link rel="stylesheet" href="css/style.css">
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
@@ -50,33 +50,35 @@
     <main>
 
         <section class="container-fluid">
-
             <div class="row justify-content-center align-items-start g-2">
+
                 <div class="col-6 col-md-5 col-xl-3 mb-2">
                     <img src="resources/angronWeb.png" class="img-fluid rounded-2" alt="Álvaro">
                 </div>
-                <div class="container col-12 col-md-12 col-xl-9">
-                    <div class="row justify-content-center align-items-center gap-5">
-                        <div class="col-11 col-md-7 bg-greenPrimary  rounded-5 p-3">
+                <div class="container-fluid col-12 col-md-7 col-xl-9">
+                    <div class="row justify-content-center align-items-center g-2">
+                        <div class="col-11 col-md-6 bg-greenPrimary shadow-sm rounded-5 p-3">
                             <h1 class="display-6 text-light nombre text-center">Álvaro Martínez Lineros</h1>
                         </div>
                         <div class="col-1 col-md-1 col-xl-1 rounded-5 p-3 bg-greenPrimary">
                             <a href="">Github</a>
                         </div>
-                    </div>
-                    <div class="row justify-content-center align-items-end p-5 g-2">
-                        <div class="col-12">
-                            <p class="fw-light fs-4 text-center p-5">
+                        <div class="col-12 col-md-12 col-xl-12 mt-5 p-3">
+                            <p class=" fw-light fs-4 text-center p-3 rounded-3 shadow-sm bg-body">
                                 ¡Hola! Me llamo Álvaro Martínez Lineros y soy desarrollador web. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum, cumque? Quisquam inventore sapiente aliquid deserunt aperiam, error vel facilis quae, minima molestias ut totam optio excepturi architecto reiciendis quos distinctio!
                             </p>
                         </div>
 
+
+
                     </div>
                 </div>
-
             </div>
         </section>
-
+        <section class="container-fluid">
+            <div class="xl-circle bg-greenPrimary"></div>
+            <div class="sm-circle bg-greenPrimary"></div>
+        </section>
     </main>
     <footer>
         <!-- place footer here -->
